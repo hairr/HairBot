58b51addc690ff6fc0b9c4918a4041021a82294a
hairr <hairrazerrr@gmail.com>
2012-11-11 01:34:44 -0600

    Updating
    mwparserfromhell

--- deadend.py
+++ deadend.py
@@ -1,4 +1,5 @@
 import mwhair
+import mwparserfromhell as mw
 import time
 import datetime
 import re
@@ -14,26 +15,40 @@ def getpages():
 def allow_bots(text, user): # [[Template:Bots#Python]]
     return not re.search(r'\{\{(nobots|bots\|(allow=none|deny=.*?' + user + r'.*?|optout=all|deny=all))\}\}', text, flags=re.IGNORECASE)
 def remove(title):
+		text = mw.parse(mwhair.edit(title))
+		oldtext = text
 		try: links = len(pagelinks(title))
 		except: links = 0
-		if links > 0:
-			text = mwhair.edit(title)
-			if allow_bots(text,'HairBot'):
-				newtext = re.sub(r'({{(D|d)ead end(\|(.*))?}}(\n)?|dead end ?=(\n)?|\| ?(d|D)ead end ?= ?([a-zA-Z0-9]+) ([a-zA-Z0-9]+))','',text)
-				newtext = re.sub(r'{{Multiple issues\|\s*{{(?P<template>[^\|]+)\|date=(?P<date>[^}]+)}}\s*}}',r'{{\g<template>\|date=\g<date>}}',newtext)
-				newtext = re.sub(r'{{Multiple issues\|\s*(?P<template>[^=]+) ?= ?(?P<date>[^\|}]+)}}',r'{{\g<template>|date=\g<date>}}',newtext)
-				if links <= 3:
-					newtext = '{{subst:dated|Underlinked}}\n' + newtext
-				save(title,newtext)
+		if links > 1:
+			if allow_bots(mwhair.edit(title),'HairBot'):
+				for template in text.filter_templates():
+					if template.name is 'Multiple issues' or 'multiple issues' or 'Article issues' or 'Articleissues' or 'Issues' or 'MI' or 'mi' or 'Mi' or 'Multiple' or 'Multiple Issues' or 'Multipleissues':
+						if template.has_param('dead end'):
+								template.remove('dead end')
+								if links <= 4:
+									text = '{{subst:dated|Underlinked}}\n' + text
+						else:
+							try:
+								for itemplate in template.get(1).value.filter_templates():
+									if itemplate.name == 'Dead end' or 'dead end' or 'DEP' or 'dep' or 'DEp' or 'DeP' or 'Dead end page' or 'dead end page' or 'dead-end' or 'Dead-end' or 'Dead-End' or 'Deadend':
+										text.remove(itemplate,'')
+										if links <= 4:
+											text = '{{subst:dated|Underlinked}}\n' + text
+							except:
+								pass
+					elif template.name is 'Dead end' or 'dead end' or 'DEP' or 'dep' or 'DEp' or 'DeP' or 'Dead end page' or 'dead end page' or 'dead-end' or 'deadend' or 'Dead-end' or 'Deadend':
+						text.replace(template,'')
+				raw_input('Press enter')
+				save(title,text)
 def save(title,text):
 	mwhair.save(title,text=text,summary='Removing dead end tag as not a valid dead end page',minor=True)
 def pagelinks(title):
-	return mwhair.links(title,limit='max',namespace='0')
+	return mwhair.links(title,namespace='0')
 def main():
 	pages = getpages()
 	for page in pages:
 		remove(page)
-		time.sleep(3)
+		time.sleep(1)
 if __name__ == '__main__':
 	mwhair.site('http://en.wikipedia.org/w/api.php')
 	mwhair.login('HairBot','password')
